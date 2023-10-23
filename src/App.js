6bacc365e596e2958949f3222a48f519578ba45a
aniketraut16 <rautan_1@rknec.edu>
2023-10-24 03:56:15 +0530

    Added Pakages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Crossword from "./components/CrossWord/Crossword";
 import Navbar from "./components/Navbar/Navbar";
+import Loginform from "./components/Loginform";
+
 function App() {
   return (
     <>
-      <Navbar />
-      <Crossword />
+      {/* <Navbar />
+      <Crossword /> */}
+      <Loginform />
     </>
   );
 }
