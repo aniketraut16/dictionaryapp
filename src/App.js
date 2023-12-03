ad23b2b76b7437f705aa90545e7402bc8fb2d9eb
aniketraut16 <rautan_1@rknec.edu>
2023-12-03 14:04:59 +0530

    Ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
 import Crossword from "./components/CrossWord/Crossword";
-import Navbar from "./components/Navbar/Navbar";
-import Loginform from "./components/Loginform";
 
 function App() {
   return (
     <>
-      {/* <Navbar />
-      <Crossword /> */}
-      <Loginform />
+      <Crossword />
     </>
   );
 }
